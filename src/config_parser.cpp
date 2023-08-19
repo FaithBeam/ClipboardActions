577a7d14653d132044ea0a7f200f27b2dcbc5e24
FaithBeam <pigeonhopper68@gmail.com>
2023-08-19 11:17:27 -0400

    remove unused files

--- src/config_parser.cpp
+++
@@ -1,4 +0,0 @@
-#include "config_parser.hpp"
-#include "parsed_apps.hpp"
-
-using namespace std;
--- src/config_writer.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include "parsed_application.hpp"
-
-void write_config(const std::vector<parsed_application*>& apps, bool notifications, const std::string& dst);
-void update_notification(bool notifications, const std::string& dst);
